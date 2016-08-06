97db2abeda1479e6e59170e694f5e286c2bcaf59
Dipak Panchal <11dipak.contact@gmail.com>
2016-08-06 23:17:10 +0530

    Update

--- db/migrate/20150610180722_create_leads.rb
+++ db/migrate/20150610180722_create_leads.rb
@@ -1,20 +1,30 @@
 class CreateLeads < ActiveRecord::Migration
   def change
-    # create_table :leads do |t|
-    #   t.string :first_name
-    #   t.string :last_name
-    #   t.string :email
-    #   t.string :work_phone
-    #   t.string :mobile_phone
-    #   t.string :home_phone
-    #   t.string :address
-    #   t.string :city
-    #   t.string :state
-    #   t.string :zipcode
-    #   t.string :type_of_home
-    #   t.string :square_footage
-
-    #   t.timestamps
-    # end
+    create_table :leads do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :work_phone
+      t.string :mobile_phone
+      t.string :home_phone
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :zipcode
+      t.string :type_of_home
+      t.string :square_footage
+      t.integer :package_id
+      t.text    :optional_coverages
+      t.string  :discount_code
+      t.string  :credit_card_number
+      t.string  :exp_date
+      t.string  :billing_address
+      t.string  :billing_name
+      t.string  :billing_city
+      t.string  :billing_state
+      t.string  :billing_zipcode
+      t.integer :payment_method
+      t.timestamps
+    end
   end
 end
--- db/migrate/20150615181041_add_column_package_id_to_lead.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnPackageIdToLead < ActiveRecord::Migration
-  def change
-    add_column :leads, :package_id, :integer
-  end
-end
