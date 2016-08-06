5fdb48f5f859be09a6f9b7f656e8b433871b1e83
Dipak Panchal <11dipak.contact@gmail.com>
2016-08-06 23:24:23 +0530

    Update

--- db/migrate/20160217032352_create_missing_renewal_dates.rb
+++ db/migrate/20160217032352_create_missing_renewal_dates.rb
@@ -1,13 +1,13 @@
 class CreateMissingRenewalDates < ActiveRecord::Migration
   def change
-    # create_table :missing_renewal_dates do |t|
-    #   t.integer :customer_id
-    #   t.string :fullname
-    #   t.string :customer_state
-    #   t.integer :no_of_transaction
-    #   t.float :total
-    #   t.string :cdate
-    #   t.timestamps
-    # end
+    create_table :missing_renewal_dates do |t|
+      t.integer :customer_id
+      t.string :fullname
+      t.string :customer_state
+      t.integer :no_of_transaction
+      t.float :total
+      t.string :cdate
+      t.timestamps
+    end
   end
 end
